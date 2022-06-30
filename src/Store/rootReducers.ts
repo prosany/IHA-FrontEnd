148c1620d1988ef4b4f9133d27a02275e5b35532
Mahabub Hasan Sany <74856571+prosany@users.noreply.github.com>
2022-07-01 01:01:15 +0600

    Remove unwanted file

--- src/Store/rootReducers.ts
+++ src/Store/rootReducers.ts
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import demoReducer from "./Demo/reducer";
 import loginReducer from "./Authentication/Login/reducer";
 import storeLoginReducer from "./Authentication/Login/storeData";
 import signupReducer from "./Authentication/Signup/reducer";
 
 const rootReducers = combineReducers({
-  demoReducer,
   loginReducer,
   storeLoginReducer,
   signupReducer,
